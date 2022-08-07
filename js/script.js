4931c95be339065a8d53fdcd117949f393c79d31
Ilmar <ilmardanilo@hotmail.com>
2022-08-07 14:04:41 -0300

    Criadas as funções Start e AddNumberToGame

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-console.log("testando..")
+let state = { board: [], currentGame: [], savedGames: [] };
+
+function start() {
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+    addNumberToGame(1)
+
+    console.log(state.currentGame);
+}
+
+function addNumberToGame(numberToAdd) {
+    if (numberToAdd < 1 || numberToAdd > 60) {
+        console.error('Número inválido', numberToAdd);
+        return;
+    }
+
+    if (state.currentGame.length > 5) {
+        console.error('O jogo já está completo.');
+        return;
+    }
+
+    state.currentGame.push(numberToAdd);
+}
+
+start();
