21aa72c1ecfc23cd346fd82d892f6032af7ea9c9
Ilmar <ilmardanilo@hotmail.com>
2022-08-07 21:36:59 -0300

    Criadas as funções removeNumberFromGame, isNumberInGame, saveGame e isGameComplete

--- js/script.js
+++ js/script.js
@@ -2,16 +2,14 @@ let state = { board: [], currentGame: [], savedGames: [] };
 
 function start() {
     addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
-    addNumberToGame(1)
+    addNumberToGame(10)
+    addNumberToGame(14)
+    addNumberToGame(21)
+    addNumberToGame(33)
+    addNumberToGame(41)
 
     console.log(state.currentGame);
+    console.log(state.savedGames);
 }
 
 function addNumberToGame(numberToAdd) {
@@ -25,7 +23,45 @@ function addNumberToGame(numberToAdd) {
         return;
     }
 
+    if (isNumberInGame(numberToAdd)) {
+        console.error('Este número já está no jogo.', numberToAdd);
+        return;
+    }
+
     state.currentGame.push(numberToAdd);
+}
+
+function removeNumberFromGame(numberToRemove) {
+    let newGame = [];
+
+    for (let index = 0; index < state.currentGame.length; index++) {
+        const currentNumber = state.currentGame[index];
+
+        if (currentNumber === numberToRemove) {
+            continue;
+        }
+
+        newGame.push(currentNumber);
+    }
+
+    state.currentGame = newGame;
+}
+
+function isNumberInGame(numberToCheck) {
+    return state.currentGame.includes(numberToCheck);
+}
+
+function saveGame() {
+    if (!isGameComplete()) {
+        console.error('O jogo não está completo.');
+        return;
+    }
+
+    state.savedGames.push(state.currentGame);
+}
+
+function isGameComplete() {
+    return state.currentGame.length === 6;
 }
 
 start();
